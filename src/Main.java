9f29745d4f0b3f23fc574240f88ab95cf3d658a1
Mohammed <mohammedalomari000@gmail.com>
2023-05-23 23:00:56 +0300

    solve this please

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,13 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+import java.util.*;
+class Main {
+    /**
+     Write a java program that validates if a string read from the console is a valid floating-point number
+     (e.g., 22.01, 2.0, 33, 0.4, .5 are all valid)
+     */
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+
+
     }
 }
